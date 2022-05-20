d429df5221abbeddf3ef82f9e24df0f35e1305ec
nsingla20 <nsingla20@iitk.ac.in>
2022-05-20 20:53:29 +0530

    added controller

---
+++ controller.py
@@ -0,0 +1,14 @@
+from nbformat import read
+import requests
+
+main_server="http://localhost:5000"
+
+nodes=requests.get(main_server+"/nodes").json().get('nodes')
+
+print("Nodes in network : "+str(nodes))
+while True:
+    sender=int(input("Sender:"))
+    rec=int(input("Recipient:"))
+    amt=int(input("Amount:"))
+    requests.post("http://"+nodes[sender]+"/pay",json={'recipient':nodes[rec],'amount':amt})
+    
